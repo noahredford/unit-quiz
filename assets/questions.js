b49551ad5e84cbd26fd0e84af79b1dc4b62c0cd6
Noah Redford <noah.redford27@gmail.com>
2023-01-05 23:09:21 -0700

    finished code

---
+++ assets/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What type of heating is at 368 La Montagna Strada?",
+        answer: "Gas Forced Air",
+        options: [
+            "Gas Forced Air",
+            "Electric Baseboard",
+            "Gas Boiler",
+            "Electric Ceiling Cable"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who owns the property at 445 McKinley Ave?",
+        answer: "David A. Hubbard",
+        options: [
+            "Dannis Adamson",
+            "Five Star Property Management",
+            "David A. Hubbard",
+            "Juice Jar LLC",
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many properties do we managage on Alamo St.?",
+        answer: "3",
+        options: [
+            "4",
+            "3",
+            "12",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following amenties are listed at 1011 Deon #1",
+        answer: "None of the Above",
+        options: [
+            "Fenced Yard",
+            "Fiber Optic Internet",
+            "Deck",
+            "None of the Above",
+        ]
+    }, 
+    {
+        numb: 5,
+        question : "How many upstairs Aparment do we manage?",
+        answer: "95",
+        options: [
+            "54",
+            "95",
+            "236",
+            "10",
+        ]
+    },
+    {
+        numb: 6,
+        question : "Who was the turn coordinator to turn 482 C Pheasant Ridge?",
+        answer: "Trent",
+        options: [
+            "Brett",
+            "Chris",
+            "Kevin",
+            "Trent",
+        ]
+    },
+    {
+        numb: 7,
+        question : "What type of dwelling is 1205 N Main St?",
+        answer: "Single Family Home",
+        options: [
+            "Single Family Home",
+            "Multi Family Home",
+            "Student Housing",
+            "Townhome",
+        ]
+    },
+    {
+        numb: 8,
+        question : "What is the square footage of 590 W Maple?",
+        answer: "2,418",
+        options: [
+            "2,043",
+            "900",
+            "2,311",
+            "3,213",
+        ]
+    },
+    {
+        numb: 9,
+        question : "Who is the owner of 724 S 19th Ave?",
+        answer: "Jason Christensen",
+        options: [
+            "Jason Christensen",
+            "Stash Investments LLC",
+            "Jay Christensen",
+            "David Hubbard",
+        ]
+    },
+    {
+        numb: 10,
+        question : "Who was the resident at 1981 Bengalview Drive in January of 2023?",
+        answer: "None of the Above",
+        options: [
+            "Jessica Hope",
+            "James Wilske",
+            "Bill Quale",
+            "None of the Above",
+        ]
+    }
+];
